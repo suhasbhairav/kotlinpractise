596d4de7bdc6728386d591c3d9e4dee67603414e
Suhas Bhairav <suhasbhairav@gmail.com>
2020-12-02 18:43:07 +0100

    functions...

--- src/ExtensionsFunctionsExamples.kt
+++ src/ExtensionsFunctionsExamples.kt
@@ -19,8 +19,18 @@ fun IntArray.subTo(dest: IntArray){
     }
 }
 
+fun add(x: Int) =  x+2
+fun sub(y: Int) =  y-2
+
 fun main() {
     val myArray = intArrayOf(1,2,3)
     intArrayOf(4,5,6).addTo(myArray)
     intArrayOf(10,20,30).subTo(myArray)
+
+    val list1 = listOf<Int>(1,2,3,4).map(::add)
+    val list2 = listOf<Int>(50,60,70,80).map(::sub)
+
+    println(list1)
+    println(list2)
+
 }
